5f5689ee87cbe7c5a347712209d4e95cffce9bc2
ilia morozov <ilia.m.composer@gmail.com>
2023-06-21 10:52:07 +0100

    changed url for dev version

--- src/contexts/baseUrl.js
+++ src/contexts/baseUrl.js
@@ -1,6 +1,6 @@
 
 //PROD MODE
-export const baseUrl = "https://trinity.jdwint.local/TrinityWebAPI/api"
+export const baseUrl = "https://testapi.etrinity.services/TrinityWebAPI/api"
 
 //DEV MODE
 // export const baseUrl = "https://testapi.etrinity.services/TrinityWebAPI/api"
