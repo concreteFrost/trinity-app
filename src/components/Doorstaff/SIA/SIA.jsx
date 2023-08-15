ab1a9c8415dd933115190226fabd0673bd764fd2
concreteFrost <ilia.m.composer@gmail.com>
2023-08-15 17:41:57 +0300

    added spinner to sia in dev branch

--- src/components/Doorstaff/SIA/SIA.jsx
+++ src/components/Doorstaff/SIA/SIA.jsx
@@ -1,29 +1,39 @@
 import s from "./SIA.module.scss"
-import { useSelector, useDispatch} from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import { FaSearch } from "react-icons/fa"
 import { GetSIAdataAPI } from "../../../redux/api/siaAPI"
-
+import { useState } from "react"
+import { TailSpin } from "react-loader-spinner";
 
 
 export const SIA = () => {
     const dispatch = useDispatch();
     const token = useSelector((state) => state.userReducer.user.access_token)
-    const sia = useSelector((state)=> state.siaReducer.siaNumber)
+    const sia = useSelector((state) => state.siaReducer.siaNumber)
+    const [isLoading, setIsLoading] = useState(false);
 
-    const handleSubmit = (e) => {
+    async function handleSubmit(e) {
         e.preventDefault()
-        console.log('submitted')
-        dispatch(GetSIAdataAPI(token,sia))
+        setIsLoading(true)
+        await dispatch(GetSIAdataAPI(token, sia))
+        await setIsLoading(false)
 
     }
     return (<div className={s.container}>
         <form onSubmit={handleSubmit} >
             <div >
                 <label htmlFor="sia">#SIA</label>
-                <input type="text" name='sia' value={sia || ''} onChange={(e)=>{dispatch({type:"SET_SIA_NUMBER", data : e.target.value})}} />
+                <input type="text" name='sia' value={sia || ''} onChange={(e) => { dispatch({ type: "SET_SIA_NUMBER", data: e.target.value }) }} />
             </div>
             <button><FaSearch></FaSearch></button>
         </form>
+        {isLoading === true ? (
+        <div className="tail_spin_container">
+          <div className="tail_spin">
+            <TailSpin width={150} height={150} color={"#42aaf5"}></TailSpin>
+          </div>{" "}
+        </div>
+      ) : null}
     </div>
     );
 }
