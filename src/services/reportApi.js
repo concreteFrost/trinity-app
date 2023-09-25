b1cfa974522deb2c93ae4a68245751a4c008486f
ilia morozov <ilia.m.composer@gmail.com>
2023-09-25 22:23:34 +0100

    finished api refactor

--- src/services/reportApi.js
+++ src/services/reportApi.js
@@ -48,40 +48,6 @@ export function GetDoorstaffRecentAPI(user, fromDate, toDate, system) {
   })
 }
 
-// export function GetAnalytics(token, fromDate, toDate, user,system) {
-//   return function (dispatch) {
-//       const _data ={
-//           dateFrom: fromDate,
-//           dateTo: toDate.split("T")[0] + "T23:59:999.000Z",
-//           locationId: parseInt(user.locationId),
-//           locationGroupId: 0,
-//           supplierId: 0,
-//           reference: 0,
-//           paymentStatusId: 0
-//       }
-//       return axios({
-//           url: baseUrl+ "/Report/ActivityList?system=" + system,
-//           headers: {
-//               Authorization: "Bearer " + token,
-//               "Content-Type": "application/json",
-//           },
-//           method: "POST",
-//           data: _data
-//       })
-//           .then((res) => {
-
-//               switch(system){
-//                   case "S":
-//                       dispatch({ type: "GET_DOORSTAFF_ANALYTICS", data: res.data.reportRecord })
-//                       break;
-//                   case "A":
-//                       dispatch({ type: "GET_COSTS_ANALYTICS", data: res.data.reportRecord })
-//                       break;                
-//               }
-
-//           })
-//   }
-// }
 
 //returns current or recent activities
 export function GetActivityAPI(token, dateFrom, dateTo) {
