a46b42bf92434e303e3070f6ee76f5dd2e1a87a3
Ilia Morozov <ilia.m.composer@gmail.com>
2025-04-23 11:21:37 +0100

    fixed activity time layout

--- src/components/Activity/AddActivity/FormElements/DateTimeElement.jsx
+++ src/components/Activity/AddActivity/FormElements/DateTimeElement.jsx
@@ -12,34 +12,38 @@ export default function DateTimeElement({ date, setDate, time, setTime }) {
         onChange={(e) => setDate(e.target.value)}
       />
       <label htmlFor="time">Select Time:</label>
-      <input
-        type="number"
-        id="hours"
-        min="0"
-        max="23"
-        placeholder="HH"
-        value={time.hours}
-        onChange={(e) =>
-          setTime((prev) => {
-            return { ...prev, hours: e.target.value };
-          })
-        }
-      />{" "}
-      :
-      <select
-        id="minutes"
-        value={time.minutes}
-        onChange={(e) =>
-          setTime((prev) => {
-            return { ...prev, minutes: e.target.value };
-          })
-        }
-      >
-        <option value="00">00</option>
-        <option value="15">15</option>
-        <option value="30">30</option>
-        <option value="45">45</option>
-      </select>
+      <div className={s.hours_minutes}>
+        <div>
+          <input
+            type="number"
+            id="hours"
+            min="0"
+            max="23"
+            placeholder="HH"
+            value={time.hours}
+            onChange={(e) =>
+              setTime((prev) => {
+                return { ...prev, hours: e.target.value };
+              })
+            }
+          />
+        </div>
+        <span>:</span>
+        <select
+          id="minutes"
+          value={time.minutes}
+          onChange={(e) =>
+            setTime((prev) => {
+              return { ...prev, minutes: e.target.value };
+            })
+          }
+        >
+          <option value="00">00</option>
+          <option value="15">15</option>
+          <option value="30">30</option>
+          <option value="45">45</option>
+        </select>
+      </div>
     </div>
   );
 }
